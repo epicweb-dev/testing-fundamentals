c597314de29b35f167e19c956f7228ce88ae8c6f
Artem Zakharchenko <kettanaito@gmail.com>
2023-12-04 15:26:21 +0100

    finish scoping test exercise

---
+++ exercises/02.test-structure/02.solution.scoping-tests/greet.js
@@ -0,0 +1,35 @@
+function greet(name) {
+	return 'Hello, ${name}!'
+}
+
+function congratulate(name) {
+	return `Congrats, ${name}!`
+}
+
+test('returns a greeting message for the given name', () => {
+	expect(greet('John')).toBe('Hello, John!')
+})
+
+test('returns a congratulation message for the given name', () => {
+	expect(congratulate('Sarah')).toBe('Congrats, Sarah!')
+})
+
+function expect(actual) {
+	return {
+		toBe(expected) {
+			if (actual !== expected) {
+				throw new Error(`Expected ${actual} to equal to ${expected}`)
+			}
+		},
+	}
+}
+
+function test(title, callback) {
+	try {
+		callback()
+		console.log(`✓ ${title}`)
+	} catch (error) {
+		console.error(`✗ ${title}`)
+		console.error(error, '\n')
+	}
+}
