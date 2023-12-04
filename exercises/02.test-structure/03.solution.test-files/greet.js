3d7e5a374d3702e1facc4ae903fa4a85b3491055
Artem Zakharchenko <kettanaito@gmail.com>
2023-12-04 15:55:43 +0100

    add test files exercise

---
+++ exercises/02.test-structure/03.solution.test-files/greet.js
@@ -0,0 +1,7 @@
+export function greet(name) {
+	return `Hello, ${name}!`
+}
+
+export function congratulate(name) {
+	return `Congrats, ${name}!`
+}
---
+++ exercises/02.test-structure/03.solution.test-files/setup.js
@@ -0,0 +1,22 @@
+function expect(actual) {
+	return {
+		toBe(expected) {
+			if (actual !== expected) {
+				throw new Error(`Expected ${actual} to equal to ${expected}`)
+			}
+		},
+	}
+}
+
+function test(title, callback) {
+	try {
+		callback()
+		console.log(`✓ ${title}`)
+	} catch (error) {
+		console.error(`✗ ${title}`)
+		console.error(error, '\n')
+	}
+}
+
+Reflect.set(globalThis, 'expect', expect)
+Reflect.set(globalThis, 'test', test)
