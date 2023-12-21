ba9e4277d91f5ed02820d77a848c59cbfaacaf5c
Artem Zakharchenko <kettanaito@gmail.com>
2023-12-21 13:07:17 +0100

    add emojis to 01-01

--- exercises/01.principles/01.problem.intentions/greet.js
+++ exercises/01.principles/01.problem.intentions/greet.js
@@ -1,3 +1,11 @@
 function greet(name) {
 	return `Hello, ${name}!`
 }
+
+// 🐨 Create a variable named "message" and assign it the result
+// of the "greet()" function call.
+// 💰 Maybe pass "John" as the name.
+
+// 🐨 Add an "if" statement to check whether the "message" equals
+// what you expect.
+// 💰 Verify the message is "Hello, John!"
