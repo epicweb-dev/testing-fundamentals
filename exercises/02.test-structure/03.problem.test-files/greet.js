40544bb95017553e2479ab24acf59f766351f40e
Artem Zakharchenko <kettanaito@gmail.com>
2024-01-19 14:38:32 +0100

    02-03: add missing kody, add marty to help with globals

--- exercises/02.test-structure/03.problem.test-files/greet.js
+++ exercises/02.test-structure/03.problem.test-files/greet.js
@@ -1,11 +1,18 @@
+// 🐨 Export the "greet()" function so it can be imported
+// and called in a newly added "greet.test.js" file.
 function greet(name) {
 	return `Hello, ${name}!`
 }
 
+// 🐨 Export the "congratulate()" function so we can use
+// it in the tests in "greet.test.js" as well.
 function congratulate(name) {
 	return `Congrats, ${name}!`
 }
 
+// 🐨 Move the existing tests for "greet()" and "congratulate()"
+// to the newly added "greet.test.js" file. Don't forget to
+// import the "test()" and "congratulate()" functions there!
 test('returns a greeting message for the given name', () => {
 	expect(greet('John')).toBe('Hello, John!')
 })
@@ -14,6 +21,8 @@ test('returns a congratulation message for the given name', () => {
 	expect(congratulate('Sarah')).toBe('Congrats, Sarah!')
 })
 
+// 🐨 Move the existing "expect()" and "test()" functions to the
+// newly added "setup.js" file.
 function expect(actual) {
 	return {
 		toBe(expected) {
@@ -24,6 +33,10 @@ function expect(actual) {
 	}
 }
 
+// 🐨 Expose the "expect()" and "test()" functions globally
+// so any test file can access them without importing them.
+// 💰 globalThis.expect = expect
+// 💰 globalThis.test = test
 function test(title, callback) {
 	try {
 		callback()
