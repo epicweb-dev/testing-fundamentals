782733aea7b92744029d46bbad4dba006b67cf29
Noah Patterson <noah@noahpatterson.me>
2024-02-26 18:44:18 -0700

    Fix user.name and user.firstName mixup

--- exercises/03.async/01.solution.await/greet.ts
+++ exercises/03.async/01.solution.await/greet.ts
@@ -6,7 +6,7 @@ export function greet(name: string) {
 
 export async function greetByResponse(response: Response) {
 	const user = await response.json()
-	return greet(user.firstName)
+	return greet(user.name)
 }
 
 export function congratulate(name: string) {
